40fa3d72f9ec062d6aa3451716847f885ece9a48
Ari Dwiantoro <aridwiantoro09@gmail.com>
2021-12-07 05:06:05 +0700

    tambah kosan form

---
+++ admin/page/room/add_room.php
@@ -0,0 +1,31 @@
+ <!-- Content Header (Page header) -->
+ <div class="content-header">
+     <div class="container-fluid">
+         <div class="row mb-2">
+             <div class="col-sm-6">
+                 <h1 class="m-0">Tambah Kosan / Kontrakan</h1>
+             </div><!-- /.col -->
+             <div class="col-sm-6">
+                 <ol class="breadcrumb float-sm-right">
+                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                     <li class="breadcrumb-item active">Tambah Kosan</li>
+                 </ol>
+             </div><!-- /.col -->
+         </div><!-- /.row -->
+     </div><!-- /.container-fluid -->
+ </div>
+ <!-- /.content-header -->
+
+ <!-- Main content -->
+ <div class="content">
+     <div class="container-fluid">
+         <div class="card">
+             <div class="card-body">
+                 Start creating your amazing application!
+             </div>
+             <!-- /.card-body -->
+         </div>
+         <!-- /.row -->
+     </div><!-- /.container-fluid -->
+ </div>
+ <!-- /.content -->
