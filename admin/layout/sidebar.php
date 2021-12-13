c991b8ff06f55ccbfbdd299ef0f15924599ee72f
Ari Dwiantoro <aridwiantoro09@gmail.com>
2021-12-13 23:30:50 +0700

    new update

--- admin/layout/sidebar.php
+++ admin/layout/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
--- admin/page/room/room.php
+++ admin/page/room/room.php
@@ -3,12 +3,12 @@
      <div class="container-fluid">
          <div class="row mb-2">
              <div class="col-sm-6">
-                 <h1 class="m-0">Tambah Kosan / Kontrakan</h1>
+                 <h1 class="m-0">Dashboad Kosan</h1>
              </div><!-- /.col -->
              <div class="col-sm-6">
                  <ol class="breadcrumb float-sm-right">
                      <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                     <li class="breadcrumb-item active">Tambah Kosan</li>
+                     <li class="breadcrumb-item active">Dashboard Kosan</li>
                  </ol>
              </div><!-- /.col -->
          </div><!-- /.row -->
@@ -19,13 +19,109 @@
  <!-- Main content -->
  <div class="content">
      <div class="container-fluid">
-         <div class="card">
-             <div class="card-body">
-                 Start creating your amazing application!
+         <a href="" class="btn btn-success mb-3"><i class="fas fa-plus"></i> Tambah Kosan</a>
+         <div class="card card-solid">
+             <div class="card-body pb-0">
+                 <div class="row">
+                     <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+                         <div class="card bg-light d-flex flex-fill">
+                             <div class="card-header text-muted border-bottom-0">
+                                 Digital Strategist
+                             </div>
+                             <div class="card-body pt-0">
+                                 <div class="row">
+                                     <div class="col-7">
+                                         <h2 class="lead"><b>Nicole Pearson</b></h2>
+                                         <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
+                                         <ul class="ml-4 mb-0 fa-ul text-muted">
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+                                         </ul>
+                                     </div>
+                                     <div class="col-5 text-center">
+                                         <img src="AdminLTE/dist/img/user1-128x128.jpg" alt="user-avatar" class="img-circle img-fluid">
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="card-footer">
+                                 <div class="text-right">
+                                     <a href="#" class="btn btn-sm bg-teal">
+                                         <i class="fas fa-comments"></i>
+                                     </a>
+                                     <a href="#" class="btn btn-sm btn-primary">
+                                         <i class="fas fa-user"></i> View Profile
+                                     </a>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+                         <div class="card bg-light d-flex flex-fill">
+                             <div class="card-header text-muted border-bottom-0">
+                                 Digital Strategist
+                             </div>
+                             <div class="card-body pt-0">
+                                 <div class="row">
+                                     <div class="col-7">
+                                         <h2 class="lead"><b>Nicole Pearson</b></h2>
+                                         <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
+                                         <ul class="ml-4 mb-0 fa-ul text-muted">
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+                                         </ul>
+                                     </div>
+                                     <div class="col-5 text-center">
+                                         <img src="AdminLTE/dist/img/user1-128x128.jpg" alt="user-avatar" class="img-circle img-fluid">
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="card-footer">
+                                 <div class="text-right">
+                                     <a href="#" class="btn btn-sm bg-teal">
+                                         <i class="fas fa-comments"></i>
+                                     </a>
+                                     <a href="#" class="btn btn-sm btn-primary">
+                                         <i class="fas fa-user"></i> View Profile
+                                     </a>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+                         <div class="card bg-light d-flex flex-fill">
+                             <div class="card-header text-muted border-bottom-0">
+                                 Digital Strategist
+                             </div>
+                             <div class="card-body pt-0">
+                                 <div class="row">
+                                     <div class="col-7">
+                                         <h2 class="lead"><b>Nicole Pearson</b></h2>
+                                         <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
+                                         <ul class="ml-4 mb-0 fa-ul text-muted">
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
+                                             <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+                                         </ul>
+                                     </div>
+                                     <div class="col-5 text-center">
+                                         <img src="AdminLTE/dist/img/user1-128x128.jpg" alt="user-avatar" class="img-circle img-fluid">
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="card-footer">
+                                 <div class="text-right">
+                                     <a href="#" class="btn btn-sm bg-teal">
+                                         <i class="fas fa-comments"></i>
+                                     </a>
+                                     <a href="#" class="btn btn-sm btn-primary">
+                                         <i class="fas fa-user"></i> View Profile
+                                     </a>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
              </div>
-             <!-- /.card-body -->
          </div>
-         <!-- /.row -->
      </div><!-- /.container-fluid -->
  </div>
  <!-- /.content -->
